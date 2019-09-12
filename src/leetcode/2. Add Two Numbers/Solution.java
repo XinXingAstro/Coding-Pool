87a96078759da6d7280de0a27ac6dac90dba7305
xing-laptop <1178252981@qq.com>
2019-09-12 17:05:29 +0800

    add 2,3

---
+++ src/leetcode/2. Add Two Numbers/Solution.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+	//这道题异常情况比较多使用以下Test case
+	//l1={9,9},l2={1}
+	//l1={},l2={0,1}
+	//l1={0,1},l2={0,1,2}
+	//Time complexity: O(max(n,m))
+	//Space complexity: O(max(n,m))
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        boolean carry = false;
+        int value = 0;
+        ListNode head = new ListNode(0);
+        ListNode node = head;
+        while (l1 != null || l2 != null || carry) {
+        	if (carry) {
+        		value = 1;
+        		carry = false;
+        	} else value = 0;
+        	if (l1 != null) {
+        		value += l1.val;
+        		l1 = l1.next;
+        	}
+        	if (l2 != null) {
+        		value += l2.val;
+        		l2 = l2.next;
+        	} 
+        	if (value >= 10) {
+        		carry = true;
+        		value -= 10;
+        	}
+        	head.next = new ListNode(value);
+        	head = head.next;
+        }
+        return node.next;
+    }
+}
---
+++ src/leetcode/3. Longest Substring Without Repeating Characters/Solution.java
@@ -0,0 +1,28 @@
+class Solution {
+	//Brute Force Search
+	//注意字符串可能为null
+	//注意字符串长度可能为1
+	//Time complexity: O(n^2)
+	//Space complexity: O(n)
+    public int lengthOfLongestSubstring_bf(String s) {
+        char str[] = s.toCharArray();
+        Map<Character, Integer> map = new HashMap<>();
+        int len = 0, ans = 0;
+        for (int i = 0; i < str.length; i++) {
+        	if (!map.containsKey(str[i])) {
+        		len++;
+        		ans = Math.max(len, ans);
+                map.put(str[i], i);
+        	} else {
+        		int first = map.get(str[i]);
+        		map.put(str[i], i);
+        		i = first;
+                len = 0;
+                map = new HashMap<>();
+        	}
+        }
+        return ans;
+    }
+
+    
+}
