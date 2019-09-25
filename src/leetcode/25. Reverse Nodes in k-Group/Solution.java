b477f8659d91ce4e0a505761e895afdf8bbb7c51
xing-laptop <1178252981@qq.com>
2019-09-25 14:24:38 +0800

    25

--- src/leetcode/25. Reverse Nodes in k-Group/Solution.java
+++ src/leetcode/25. Reverse Nodes in k-Group/Solution.java
@@ -7,26 +7,33 @@
  * }
  */
 class Solution {
+	//速度很快，空间占用比较多
     public ListNode reverseKGroup(ListNode head, int k) {
         if (head == null || head.next == null) return head;
         ListNode pre = new ListNode(0);
         pre.next = head;
         ListNode[] node = new ListNode[k + 1];
         node[1] = pre;
-        while (node[0] != null && node[0].next != null) {
-	        //move
-	        node[0] = node[1];
-	        node[1] = node[1].next;
-	        for (int i = 2; i <= k; i++) {
-	        	node[i] = node[i - 1].next;
-	        }
-	        //reverse
-	        node[0].next = node[k];
-	        node[1].next = node[k].next;
-	        for (int j = 1; j < (k - 1); j++) {
-	        	node[j + 1].next = node[j];
-	        }
-        }
+        while (move(node, k))
+        	reverse(node, k);
         return pre.next;
+    }
+    public void reverse(ListNode[] node, int k) {
+        if (node[k] == null) return;
+    	node[0].next = node[k];
+    	node[1].next = node[k].next;
+    	for (int i = 2; i <= k; i++) {
+    		node[i].next = node[i - 1];
+    	}
+    }
+    public boolean move(ListNode[] node, int k) {
+    	if (node[1] == null) return false;
+    	node[0] = node[1];
+    	node[1] = node[1].next;
+    	for (int i = 2; i <= k; i++) {
+    		if (node[i - 1] == null) return false;
+    		node[i] = node[i - 1].next;
+    	}
+    	return true;
     }
 }
