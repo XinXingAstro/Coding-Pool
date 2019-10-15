e05c7afd48a1decf3538c07bc515b74c339f1e33
xing-laptop <1178252981@qq.com>
2019-10-15 11:38:32 +0800

    28

--- src/leetcode/28. Implement strStr()/Solution.java
+++ src/leetcode/28. Implement strStr()/Solution.java
@@ -20,4 +20,15 @@ class Solution {
         }
         return -1;
     }
+
+    //Discuss most votes
+    public int strStr(String  haystack, String needle) {
+        for (int i = 0; ; i++) {
+            for (int j = 0; ; j++) {
+                if (j == needle.length()) return i;
+                if (i + j == haystack.length()) return -1;
+                if (needle.charAt(j) != haystack.charAt(i + j)) break;
+            }
+        }
+    }
 }
