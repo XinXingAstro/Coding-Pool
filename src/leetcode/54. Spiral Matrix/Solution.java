6bc8b66824ae35f7474fa3972f8a469c9097477b
xing-laptop <1178252981@qq.com>
2019-11-04 14:41:40 +0800

    54

--- src/leetcode/54. Spiral Matrix/Solution.java
+++ src/leetcode/54. Spiral Matrix/Solution.java
@@ -2,14 +2,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
+    /**
+     * Spiral order list.
+     * 使用used矩阵记录每个元素是否访问过，while+条件判断转变方向
+     *
+     * @param matrix the matrix
+     * @return the list
+     */
     public List<Integer> spiralOrder(int[][] matrix) {
         if (matrix.length == 0 || matrix[0].length == 0) return new ArrayList<>();
         List<Integer> ans = new ArrayList<>();
-        int m = matrix.length, n = matrix[0].length, row=0, col=0;
+        int m = matrix.length, n = matrix[0].length, i=0, row=0, col=0;
         boolean[][] used = new boolean[m][n];
         ans.add(matrix[row][col]);
         used[row][col] = true;
-        for (int i = 0; i <= (m * n); i++) {
+        while (i < (m * n - 1)) {
             while (col+1 < n && !used[row][col+1]) {
                 col++;
                 ans.add(matrix[row][col]);
