74a599eb639a8fe5516c2454e47d7d18a5e16b4b
xing-laptop <1178252981@qq.com>
2019-11-13 17:04:56 +0800

    update 66

--- src/leetcode/66. Plus One/Solution.java
+++ src/leetcode/66. Plus One/Solution.java
@@ -25,4 +25,25 @@ public class Solution {
             ans[list.size()-1-i] = list.get(i);
         return ans;
     }
+
+    /**
+     * Plus one int [ ].
+     * 看别人的代码多简洁
+     *
+     * @param digits the digits
+     * @return the int [ ]
+     */
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n-1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] ans = new int[n + 1];
+        ans[0] = 1;
+        return ans;
+    }
 }
