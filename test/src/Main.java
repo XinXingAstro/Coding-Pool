bca16001b0eeb95910d08edfb8c915b2f2b89b29
xing-laptop <1178252981@qq.com>
2019-12-09 15:58:06 +0800

    add test module

---
+++ test/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+        ListNode d = new ListNode(4);
+        ListNode e = new ListNode(5);
+        ListNode f = new ListNode(6);
+        ListNode g = new ListNode(7);
+        ListNode h = new ListNode(8);
+        ListNode i = new ListNode(9);
+        ListNode j = new ListNode(10);
+        a.next = b; b.next = c; c.next = d; d.next = e; e.next = f; f.next = g; g.next = h;
+        h.next = i; i.next = j;
+        new Solution().reverseBetween(a, 1, 10);
+//        System.out.println(new Solution().partition(a, 3));
+        /*String s = "226";
+        System.out.println(new Solution().numDecodings(s));*/
+    }
+}
