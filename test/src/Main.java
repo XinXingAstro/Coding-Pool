64a79bd85ada3f752731204871612a24dee2d910
xing-desktop <1178252981@qq.com>
2020-04-14 23:11:25 +0800

    update

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        new MinStack();
 /*        ListNode a = new ListNode(1);
         ListNode b = new ListNode(2);
         ListNode c = new ListNode(3);
@@ -15,17 +14,25 @@ public class Main {
         h.next = i; i.next = j;*/
         /*TreeNode a = new TreeNode(1);
         TreeNode b = new TreeNode(2);
-        TreeNode c = new TreeNode(2);
-        TreeNode d = new TreeNode(3);
-        TreeNode e = new TreeNode(4);
-        TreeNode f = new TreeNode(4);
-        TreeNode g = new TreeNode(3);
-        a.left = b; a.right = c; b.left = d; b.right = e; c.left = f; c.right = g;
-        System.out.println(new Solution().sumNumbers(a));*/
+        TreeNode c = new TreeNode(3);
+        TreeNode d = new TreeNode(4);
+        TreeNode e = new TreeNode(5);
+        TreeNode f = new TreeNode(6);
+        TreeNode g = new TreeNode(7);
+        a.left = b; a.right = c; b.left = d; b.right = e; *//*c.left = f; *//*c.right = g;
+        System.out.println(new Solution().rob(a));*/
 //        System.out.println(new Solution().isPalindrome("A man, a plan, a canal: Panama"));
         /*String s = "226";
         System.out.println(new Solution().numDecodings(s));*/
 //        System.out.println(new Test().test("123"));
 
+        TreeNode a = new TreeNode(5);
+        TreeNode b = new TreeNode(3);
+        TreeNode c = new TreeNode(6);
+        TreeNode d = new TreeNode(2);
+        TreeNode e = new TreeNode(4);
+        TreeNode f = new TreeNode(7);
+        a.left = b; a.right = c; b.left = d; b.right = e; c.right = f;
+        new Solution().deleteNode(a, 3);
     }
 }
--- test/src/Solution.java
+++ test/src/Solution.java
@@ -1,18 +1,48 @@
-import java.util.*;
-public class Solution {
-    public int findMaxLength(int[] nums) {
-        if (nums == null || nums.length == 0) return 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        int ans = 0, sum = 0;
-        map.put(0, -1);
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i] == 1 ? 1 : -1;
-            if (map.containsKey(sum)) {
-                ans = Math.max(ans, i - map.get(sum));
-            } else {
-                map.put(sum, i);
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return root;
+        // find Key node
+        TreeNode p = root;
+        TreeNode parent = root;
+        while (p.val != key) {
+            if (key > p.val) {
+                parent = p;
+                p = p.right;
+            }
+            else if (key < p.val) {
+                parent = p;
+                p = p.left;
             }
         }
-        return ans;
+        // delete p
+        if (p.left == null && p.right == null) {
+            if (parent.val > p.val) parent.left = null;
+            else parent.right = null;
+        } else if (p.right == null) {
+            if (parent.val > p.val) parent.left = p.left;
+            else parent.right = p.left;
+        } else if (p.left == null) {
+            if (parent.val > p.val) parent.left = p.right;
+            else parent.right = p.right;
+        } else {
+            TreeNode t = p.right, tparent = null;
+            while (t.left != null) {
+                tparent = t;
+                t = t.left;
+            }
+            p.val = t.val;
+            if (tparent != null) tparent.left = t.right;
+            else p.right = t.right;
+        }
+        return root;
     }
 }
