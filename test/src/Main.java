8c771bb615a8f36abbdb85fb837d9114d208f093
xing-desktop <1178252981@qq.com>
2020-05-09 21:53:35 +0800

    update

--- test/src/Main.java
+++ test/src/Main.java
@@ -67,6 +67,9 @@ public class Main {
         // String s = "egg";
         // String t = "add";
         // new Solution().isIsomorphic(s, t);
+        char[][] b = {{'a','b','c'},{'a','e','d'},{'a','f','g'}};
+        String[] w = {"eaabcdgfa"};
+        new Solution().findWords(b, w);
 
     }
 
