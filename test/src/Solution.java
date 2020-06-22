15053f2d4da5c7e26f6e9cc117239e7bd92c9da8
xing-laptop <1178252981@qq.com>
2020-06-22 17:04:40 +0800

    update

--- test/src/Solution.java
+++ test/src/Solution.java
@@ -1,13 +1,19 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        if (num <= 1) return true;
+        if (num <= 1) {
+            return true;
+        }
         int lo = 0, hi = num/2;
         while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
-            double sqr = (double)mid * (double)mid;
-            if (sqr == (double)num) return true;
-            else if (sqr < (double)num) lo = mid + 1;
-            else hi = mid - 1;
+            double sqr = (double) mid * (double) mid;
+            if (sqr == (double) num) {
+                return true;
+            } else if (sqr < (double) num) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
         }
         return false;
     }
