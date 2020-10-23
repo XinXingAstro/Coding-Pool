d101cf84c2034f0aeff98c619483d29339e2f043
xing-laptop <1178252981@qq.com>
2020-10-23 17:01:15 +0800

    Create SPFA.java

---
+++ DS&A/src/Graph/SPFA.java
@@ -0,0 +1,9 @@
+package Graph;
+/*
+* 解决：单源最短路径问题
+* Shortest Path Faster Algorithm (SPFA) 算法
+* 时间复杂度：O(kE)(k为所有顶点进队得平均次数，可以证明k一般<=2, E为图的边数)
+* 备注：Bellman-Ford算法的队列优化版本
+* */
+public class SPFA {
+}
