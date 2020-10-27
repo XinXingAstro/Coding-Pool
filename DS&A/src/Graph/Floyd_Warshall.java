edd0d538bafd4f42036880a288f0dff2aac41b3f
xing-laptop <1178252981@qq.com>
2020-10-27 17:04:26 +0800

    Update Floyd_Warshall.java

--- DS&A/src/Graph/Floyd_Warshall.java
+++ DS&A/src/Graph/Floyd_Warshall.java
@@ -9,7 +9,7 @@ public class Floyd_Warshall {
     private int[][] d; //d[u][v]表示边e=(u, v)的权值（不存在时设为INF，不过d[i][i]=0）
     private int V;     // 顶点数
     
-    private void warshall_floyd() {
+    private void floyd_warshall() {
         for (int k = 0; k < V; k++) {
             for (int i = 0; i < V; i++) {
                 for (int j = 0; j < V; j++) {
